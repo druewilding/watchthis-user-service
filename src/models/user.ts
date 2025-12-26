d5789986103f8d2432912231a1a81a48ac0674e6
aimee rivers <aimee@aimeerivers.com>
2025-12-26 12:35:03 +0100

    fix: Import the client correctly

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,7 +1,7 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import bcrypt from "bcrypt";
 
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from "../generated/prisma/client.js";
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
